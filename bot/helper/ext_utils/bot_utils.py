462e7fce1bcd841d408c0d533b23ff70ce6fdade
Zeno <144168045+Zeno79@users.noreply.github.com>
2024-06-01 19:20:57 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -195,7 +195,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>Powered by Aeon</b>\n\n'
+    msg = '<b>Powered by Netflix Dual</b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
